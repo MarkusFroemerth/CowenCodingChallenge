fa5e78da903152ad676b9e9c78cef0d6906194eb
Markus Frömerth <24446132+voyager75000@users.noreply.github.com>
2021-10-31 19:03:54 +0100

    CWS-2 Added one functional Jest-Test and dependencies to run it

---
+++ cowen-coding-challenge/src/app/modules/album-selector/album-selector.component.spec.ts
@@ -0,0 +1,23 @@
+import { AlbumSelectorComponent } from "./album-selector.component"
+
+describe('AlbumSelectorComponent', () => {
+    let fixture: AlbumSelectorComponent;
+
+    beforeEach(() => {
+        fixture = new AlbumSelectorComponent();
+    });
+
+    describe('Setup component', () => {
+        describe('ngOnInit', () => {
+            it('should call fetchAlbums with this.userId', () => {
+                let fetchAlbumsSyp = jest.spyOn(fixture, 'fetchAlbums');
+                const userId = 1;
+                fixture.userId = userId;
+
+                fixture.ngOnInit();
+
+                expect(fetchAlbumsSyp).toHaveBeenCalledWith(userId);
+            })
+        })
+    })
+})
