fd531514a7170ca1546ccb6aad0c3066bbad9164
Markus Frömerth <24446132+voyager75000@users.noreply.github.com>
2021-10-31 17:32:25 +0100

    CWS-4 New Albums can be created for a given user. All albums have a default name, user should be given the possibility to change / choose a name in a future ticket.

--- cowen-coding-challenge/src/app/shared/models/album.model.ts
+++ cowen-coding-challenge/src/app/shared/models/album.model.ts
@@ -1,5 +1,10 @@
 export class Album {
     declare public id: number;
-    declare public userId: number;
+    declare public userId: Number;
     declare public title: string;
+
+    constructor(userId: Number, title: string) {
+        this.userId = userId;
+        this.title = title;
+    }
 }
