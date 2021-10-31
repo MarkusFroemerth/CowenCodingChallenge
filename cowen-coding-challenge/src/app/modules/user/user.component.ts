55e839623536a2bb2aa7bdcebe04f494096bd3c1
Markus Frömerth <24446132+voyager75000@users.noreply.github.com>
2021-10-31 17:32:25 +0100

    CWS-4 New Albums can be created for a given user. All albums have a default name, user should be given the possibility to change / choose a name in a future ticket.

--- cowen-coding-challenge/src/app/modules/user/user.component.ts
+++ cowen-coding-challenge/src/app/modules/user/user.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import {Component, Input, OnInit} from '@angular/core';
 import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { Album } from 'src/app/shared/models/album.model';
 import { User } from 'src/app/shared/models/user-model';
 import { AlbumComponent } from '../album/album.component';
 
@@ -23,8 +25,33 @@ export class UserComponent implements OnInit {
 
     ngOnInit(): void {}
 
-    onShowAlbums(userId: Number) {
+    onShowAlbums() {
         let dialogRef = this.modalService.open(AlbumComponent, this.ngbModalOptions);
-        dialogRef.componentInstance.userId = userId;
+        dialogRef.componentInstance.userId = this.user.id;
+    }
+
+    onCreateAlbum() {
+        // TODO let user choose name
+        let newAlbumName = 'FooBarAlbum';
+
+        let newAlbum = new Album(this.user.id, newAlbumName);
+        fetch('https://jsonplaceholder.typicode.com/albums', {
+            method: 'POST',
+            body: JSON.stringify(newAlbum),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        }).then(response => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        }).then(json => {
+            console.log(json);
+            alert('Album successfully created');
+        }).catch(error => {
+            console.log(error);
+            alert('Album erstellen fehlgeschlagen');
+        });
     }
 }
