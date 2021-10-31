c0757f258b23a7dca101270ebc48e4f425e49b1e
Markus Frömerth <24446132+voyager75000@users.noreply.github.com>
2021-10-31 13:27:33 +0100

    CWS-1 Add user detail. Add basic styling.

--- cowen-coding-challenge/src/app/modules/user/user.component.ts
+++ cowen-coding-challenge/src/app/modules/user/user.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import { User } from 'src/app/shared/models/user-model';
 
 @Component({
     selector: 'app-user',
@@ -6,6 +7,10 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+
+    @Input()
+    public user: User = new User();
+
     constructor() {}
 
     ngOnInit(): void {}
--- cowen-coding-challenge/src/app/shared/shared.module.ts
+++ cowen-coding-challenge/src/app/shared/shared.module.ts
@@ -1,7 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { UserComponent } from '../modules/user/user.component';
-import { UserListComponent } from '../modules/user-list/user-list.component';
 
 @NgModule({
     declarations: [],
